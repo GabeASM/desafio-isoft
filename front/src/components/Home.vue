94146df53974e09cd90a3aa8cc0601ac018e442b
DanRuizK57 <d.ruizk03@ufromail.cl>
2023-05-08 17:19:08 -0400

    Vista home añadida y brevemente configurada

---
+++ front/src/components/Home.vue
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<template>
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Desafio Isoft</title>
+    </head>
+    <body>
+
+        <div>
+            <h2>Generar Autos</h2>
+            <span for="numAutos">Cantidad de autos a generar: </span>
+            <input id="numAutos" type="number">
+        </div>
+
+        <div>
+            <h2>Buscar por:</h2>
+            <span>[Filtros]</span>
+        </div>
+
+        <div>
+            <h2>Ver características de un auto</h2>
+            <span>Seleccionar Auto: </span>
+            <span>[Autos]</span>
+        </div>
+    </body>
+    </html>
+</template>
+
+<style>
+
+</style>
