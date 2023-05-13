c938986942278e98935078b802d1625e0080c203
GabeASM <g.aillapan01@ufromail.cl>
2023-05-13 11:34:51 -0400

    mas validadores agregados

--- backend/apicar/src/main/java/ufro/apicar/controller/AutoController.java
+++ backend/apicar/src/main/java/ufro/apicar/controller/AutoController.java
@@ -8,17 +8,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ufro.apicar.services.IAutoService;
 
+
 @RestController
 public class AutoController {
-
     @Autowired
     private IAutoService autoService;
 
     @GetMapping("/{cantidad}")
-    public ResponseEntity<?> listarAutos(@PathVariable int cantidad){
-        if(cantidad > 300 ) return ResponseEntity.badRequest().body("Cantidad de autos no puede ser mayor a 300");
+    public ResponseEntity<?> listarAutos(@PathVariable String cantidad){
+        try{
+            return getResponseEntityAutos(cantidad);
+        }catch (NumberFormatException e){
+            return ResponseEntity.badRequest().body("La cantidad debe ser un numero valido");
+        }
+    }
+
+    private ResponseEntity<?> getResponseEntityAutos(String cantidad) {
+        Integer cantidadInt = Integer.parseInt(cantidad);
+        ResponseEntity<?> body = validarCantidad(cantidadInt);
+        if (body != null) return body;
+        return ResponseEntity.ok(autoService.listarAutos(cantidadInt));
+    }
+
+    private ResponseEntity<?> validarCantidad(Integer cantidadInt) {
+        if(cantidadInt < 0 ) return ResponseEntity.badRequest().body("La cantidad debe ser un numero valido");
         if(autoService.retornarCantidadLista() > 300) return ResponseEntity.ok(autoService.retornarLista());
-        return ResponseEntity.ok(autoService.listarAutos(cantidad));
+        if(cantidadInt > 300 ) return ResponseEntity.badRequest().body("Cantidad de autos no puede ser mayor a 300");
+        return null;
     }
 
 }
